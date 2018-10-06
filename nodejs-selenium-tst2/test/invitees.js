817488b072a46fca5b13f722c781c5c195f6539e
Ade Akinwale <adebambo.akinwale@gmail.com>
2018-10-06 14:03:21 +0100

    page object model update

--- nodejs-selenium-tst2/test/invitees.js
+++ nodejs-selenium-tst2/test/invitees.js
@@ -2,19 +2,47 @@ const {Browser, By, Key, until } = require("selenium-webdriver");
 const {suite} = require("selenium-webdriver/testing");
 const assert = require("assert");
                require("chromedriver");
+               const RsvpPage = require('../pages/rsvp.js')
 
-const url = "https://treehouse-projects.github.io/selenium-webdriver-intermediate/waits/app/index.html";
+
+
+
 
 suite(function(env){
-    describe('RSVP site', function(){
-        it('have invitee list', function(){
-            env.builder().build()
-               .then(driver => {
-                   driver.get(url)
-                   .then(() => driver.findElements(By.id('invitedList')))
-                   .then(elements => assert(elements.lemgth > 0 ))
-                   .then(() => driver.quit());
-               })
+    describe('RSVP site',  function(){
+        let driver;
+        let page;
+        this.timeout(20000);
+        
+        //Setup
+        before(async function (){
+            driver = await env.builder().build();
+            page = new RsvpPage(driver);
+            await page.open();
+        })
+        
+        it('Has invitee list', async function(){
+           let elements = await driver.findElements(page.locators.invitedList);
+           assert(elements.length > 0);
+           
+        })
+
+        it('Has a registration list', async function(){
+            let elements = await driver.findElements(page.locators.registrationForm);
+            assert(elements.length > 0);
+            
+         })
+
+         it('Loads existing invitations', async function (){
+            //implicit waite
+            //await driver.manage().setTimeouts({implicit: 3000}); 
+            let invitees = await driver.findElements(page.locators.invitees);
+            let text = await invitees[1].getText();
+            assert(text.includes("Craig Dennis"));
+         })
+        //Tear down
+        after(async function(){
+            driver.quit();
         })
     })
 });
